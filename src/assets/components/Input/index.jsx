25c1766d44d97e81459de8487124d3612b8de0a2
Mateus Carvalho <mateuscarvalhodev@outlook.com>
2022-11-18 13:59:52 -0300

    indexComponent

---
+++ src/assets/components/Input/index.jsx
@@ -0,0 +1,15 @@
+import { Container } from "./styles";
+
+export default function Input ({id, name, label}) {
+
+  
+
+  return (
+    <>
+    <Container>
+    <input type="text" placeholder='digite aqui tal coisa'/>
+    {/* <label htmlFor={id}>teste</label> */}
+    </Container>
+    </>
+  )
+}
