b7e5093bbc36490f45f7151934db2458685fcc67
ItsCamyx <96155623+ItsCamyx@users.noreply.github.com>
2022-11-19 19:46:33 -0300

    HotFix: Adjust files and folders

--- src/assets/components/Input/index.jsx
+++ src/assets/components/Input/index.jsx
@@ -1,12 +1,11 @@
 import { Container } from "./styles";
 
-export default function Input ({id, name, label}) {
+export default function Input({ id, name, label }) {
   return (
     <>
-    <Container>
-    <input type="text" placeholder='digite aqui tal coisa'/>
-    {/* <label htmlFor={id}>teste</label> */}
-    </Container>
+      <Container>
+        <input type="text" placeholder="digite aqui tal coisa" />
+      </Container>
     </>
-  )
+  );
 }
