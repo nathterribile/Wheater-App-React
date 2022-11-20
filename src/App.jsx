9c2e2d2056b44d6f6b15483207001eeae4f756bc
ItsCamyx <96155623+ItsCamyx@users.noreply.github.com>
2022-11-19 21:42:17 -0300

    Fix: Adjust routes and folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import Home from "./assets/pages/Home";
-import Testandorotas from './assets/pages/Testerotas';
+import MainRoutes from "./routes/mainRoutes";
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/testerotas" element={<Testandorotas />} />
-      </Routes>
-    </Router>
-    
-    
+      <MainRoutes />
     </>
-  ) 
-  
+  );
 }
 
 export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import Input from "./components/Input";
+
+export default function Home() {
+  return (
+    <>
+      <Input />
+    </>
+  );
+}
