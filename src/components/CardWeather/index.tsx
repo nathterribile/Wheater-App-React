e4fb00412318881f2e313bd629ef774b052c4120
Nathalia Terribile <nathterribile@gmail.com>
2022-12-11 12:41:04 -0300

    integração da API no componente CardWeather

--- src/components/CardWeather/index.tsx
+++ src/components/CardWeather/index.tsx
@@ -1,15 +1,36 @@
 import Compass from '../../assets/44.svg'
 import icon from './../../assets/01d.svg'
-import {
-  ContainerSpace,
-  ContainerWeatherData,
-  Content,
-  ContentDetailsRight,
-  ContentImgLeft,
-  ImgTitleContainer,
-  TitleContainer,
-} from './styled'
+import { useState, useEffect } from 'react'
+import { api } from '../../services/api'
+import moment from 'moment'
+import { ContainerSpace, ContainerWeatherData, Content, ContentDetailsRight, ContentImgLeft, ImgTitleContainer,TitleContainer } from './styled'
 export default function CardWeather () {
+  const [previsao, setPrevisao] = useState([])
+  const [data, setData] = useState('')
+  const [nome, setNome] = useState('')
+  const [temperatura, setTemperatura] = useState('')
+  const [umidade, setUmidade] = useState('')
+  const [vento, setVento] = useState('')
+  const [pressao, setPressao] = useState('')
+  useEffect(() => {
+    api.get('weather', {
+      params: {
+        lon: '-0.1257',
+        lat: '51.5085',
+        lang: 'pt_br',
+        units: 'metric',
+        appid: 'edc7ecdd8e2ba0408a1c09af3d42884d' }}
+    ).then((response) => {
+      console.log(response)
+      setData(moment(Date(response.data.dt)).format('DD MM YYYY'))
+      setPrevisao(response.data.weather)
+      setNome(response.data.name)
+      setTemperatura(response.data.main.temp)
+      setUmidade(response.data.main.humidity)
+      setVento(response.data.wind.speed)
+      setPressao(response.data.main.pressure)
+    })
+  }) 
   return (
     <>
       <Content>
@@ -19,8 +40,11 @@ export default function CardWeather () {
           </ImgTitleContainer>
 
           <div>
-            <p>Previsão do tempo</p>
-            <p>Cidade</p>
+            <p>{previsao.map(item =>
+            <p key={item.description}>{item.description}</p>
+              )}
+            </p>
+            <p>{nome}</p>            
           </div>
         </TitleContainer>
 
@@ -30,16 +54,13 @@ export default function CardWeather () {
           </ContentImgLeft>
           <ContentDetailsRight>
             <ContainerSpace>
-              <p>Hoje</p>
-              <p> 20°C </p>
+              <p>{data}</p>
+              <p>Temperatura: {temperatura}°C </p>
             </ContainerSpace>
-            <div>
-              <p> Céu Limpo</p>
-            </div>
             <ContainerSpace>
-              <p>Vento: 40 - KM/H</p>
-              <p>Umidade: %</p>
-              <p>Pressão: hPA</p>
+              <p>Vento: {vento}Km/h</p>
+              <p>Umidade: {umidade}%</p>
+              <p>Pressão: {pressao}hPA</p>
             </ContainerSpace>
           </ContentDetailsRight>
         </ContainerWeatherData>
