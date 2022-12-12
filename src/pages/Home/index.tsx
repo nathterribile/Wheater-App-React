ddefa622b374b3e83b091cbfd475afc93a72cf62
mateuscarvalhodev <mateuscarvalhodev@outlook.com>
2022-12-12 10:16:03 -0300

    api conectada

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,50 @@
+import { useState } from 'react'
 import CardWeather from '../../components/CardWeather'
 import Input from '../../components/Input'
 
 function Home () {
+  const [city, setCity] = useState('')
+  const [watherForecast, setWatherForecast] = useState(null)
+  const handleChange = (e: any) => {
+    setCity(e.target.value)
+  }
+
+  const handleSearch = () => {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=e751ea9cf5bc4fcbbb3225428211810&q=${city}&lang=pt`).then((response) => {
+      if (response.status === 200) {
+        return response.json()
+      }
+    }).then((data) => {
+      setWatherForecast(data)
+      setCity('')
+    })
+  }
+
   return (
     <>
-      <Input id='' name='' placeholder='' type='' />
-      <CardWeather />
+      <input value={city} type='' onChange={handleChange} />
+      <button onClick={handleSearch}>click</button>
+      {watherForecast ? (
+        <div>
+          <div> 
+
+          <h4>
+            {watherForecast.location.name}
+          </h4>
+          <p>
+            {watherForecast.current.condition.text}
+          </p>
+          </div>
+          <div>
+            {watherForecast.current.temp_c}
+          </div>
+          <div>
+            <img src={watherForecast.current.condition.icon} alt="" />
+          </div>
+        </div>
+      ): null}
+      {/* <CardWeather /> */}
     </>
   )
 }
-export default Home
+export default Home;
