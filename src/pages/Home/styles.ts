51837821a195ce9c80bbfb08db80c215679b51fd
Camila Correia <96155623+ItsCamyx@users.noreply.github.com>
2022-12-14 18:02:06 -0300

    fix: delete unused files

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -11,6 +11,11 @@ h1 {
 .firstP {
   font-size: 24px;
 }
+.containerSearch{
+  display: flex;
+  justify-content:space-around;
+  width: 500px;
+}
 p {
   font-size: 20px;
   margin-top: 48px;
@@ -19,28 +24,32 @@ p {
 
 
 input {
-
-width: 444px;
+width: 450px;
 height: 44px;
 left: 437px;
 top: 487px;
-
 background: #EAE7EE;
 border: 1px solid #73ADC1;
+
 box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-border-radius: 18px;
-padding-right: 50px;
+border-radius: 18px 0px 0px 18px;
+padding-left: 16px;
 }
 input:focus {
   box-shadow: 0 0 0 0;
   outline: 0;
 }
 button {
-  
+  display: flex;
+  justify-content:center;
+  align-items:center;
+  height: 44px;
+  width: 50px;
   box-shadow: 0 0 0 0;
   outline: 0;
-  margin-left: -50px;
-  background: none;
+  background-color: #EAE7EE;
+  border-radius: 0px 18px 18px 0px;
+  border: 1px solid #73ADC1;
   border: none;
 }
 
